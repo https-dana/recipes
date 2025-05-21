477c3ec27051e89af9495dd75db93e02c4bc7757
Dana <bogdana.kolesnyk@gmail.com>
2025-05-21 12:42:06 +0300

    Configure environment variables for Django settings and add VSCode configuration for pytest

--- project_recipe/settings.py
+++ project_recipe/settings.py
@@ -1,3 +1,7 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()  
+
 """
 Django settings for project_recipe project.
 
@@ -19,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9jo7t)*7&hff$4x9j%37qy(o+&ywxxm@7*@b##!4#f%kxg(_-l'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-default")
+DEBUG = os.getenv("DEBUG", "False") == "True"
+
+
 
 ALLOWED_HOSTS = []
 
