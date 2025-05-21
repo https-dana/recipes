53cea4fe0918f6883fdef9f93c5d30cd2f1de119
Dana <bogdana.kolesnyk@gmail.com>
2025-05-21 12:49:45 +0300

    Add include for recipe URLs in the main URL configuration

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from recipe import views
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main, name='main'),
+    path('categories/', views.category_list, name='category_list'),
+    path('recipes/', views.recipe_list, name='recipe_list'),
+    path('recipes/<int:category_id>/', views.recipe_list, name='recipe_by_category'),
 ]
