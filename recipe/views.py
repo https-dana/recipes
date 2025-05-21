e837c9839d3939c4ed293f6e3755bbb115e82bab
Dana <bogdana.kolesnyk@gmail.com>
2025-05-21 12:46:19 +0300

    Add views for category list and recipe list

--- recipe/views.py
+++ recipe/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from .models import Category
-
+from .models import Category, Recipe 
 
 def main(request):
     categories = Category.objects.all()
     return render(request, 'main.html', context={'categories': categories})
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'recipe/category_list.html', {'categories': categories})
+
+def recipe_list(request, category_id=None):
+    if category_id:
+        recipes = Recipe.objects.filter(category_id=category_id)
+    else:
+        recipes = Recipe.objects.all()
+    return render(request, 'recipe/recipe_list.html', {'recipes': recipes})
